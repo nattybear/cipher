3f7cb8f1e3f33f86cfbe2cdaafc42839eed00ab0
Vincent <vpark45@gmail.com>
2022-06-30 15:03:35 +0000

    Add uncaesar function

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -7,3 +7,6 @@ caesar n c
   | isLetter c = chr $ (ord c + n - a) `mod` 26 + a
   | otherwise  = c
   where a = if isLower c then ord 'a' else ord 'A'
+
+uncaesar :: Int -> Char -> Char
+uncaesar = caesar . negate
