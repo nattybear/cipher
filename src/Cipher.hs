565ff2bf83156da22c6a775fe8fb87d5299052cc
Vincent <vpark45@gmail.com>
2022-06-30 14:58:06 +0000

    Write caesar function

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,1 +1,9 @@
 module Cipher where
+
+import Data.Char
+
+caesar :: Int -> Char -> Char
+caesar n c
+  | isLetter c = chr $ (ord c + n - a) `mod` 26 + a
+  | otherwise  = c
+  where a = if isLower c then ord 'a' else ord 'A'
